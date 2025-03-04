e10c2c416a043c2f770ebc5235ef576f2cb01548
Jardiparadis <justin.fournier@epitech.eu>
2025-03-04 15:11:46 +0100

    Create text widget

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,11 +1,66 @@
 <script setup lang="ts">
-  const test = 'Hello vuetify';
+  import { ref, onMounted } from 'vue';
+  import TextWidget from './TextWidget.vue';
+  const savedTime = ref('');
+
+  onMounted(() => {
+    savedTime.value = '16 j 12 h 45 min';
+  });
 </script>
 
 <template>
-  <v-btn>{{test}}</v-btn>
+  <div class="grid">
+    <TextWidget title="Temps gagné" icon="mdi-timer-check-outline" class="time-saved-widget">
+      <span class="font-weight-bold text-h5">
+        {{ savedTime }}
+      </span>
+      <div style="margin-top: 1rem;">
+        Temps de traitement estimé sans Keep'in:
+        <div class="font-weight-bold text-h6">
+          32j 18h 20 min
+        </div>
+      </div>
+    </TextWidget>
+
+    <TextWidget title="Temps gagné" class="b">
+      <div style="font-weight: bold">
+        {{ savedTime }}
+      </div>
+      <div>
+        Temps de traitement estimé sans Keep'in: 32j 18h 20 min
+      </div>
+    </TextWidget>
+    <TextWidget title="Temps gagné" class="c">
+      <div style="font-weight: bold">
+        {{ savedTime }}
+      </div>
+      <div>
+        Temps de traitement estimé sans Keep'in: 32j 18h 20 min
+      </div>
+    </TextWidget>
+  </div>
 </template>
 
 <style scoped>
+.grid {
+  margin: 20px;
+  display: grid;
+  gap: 12px;
+  grid-template-areas:
+    "TimeSaved LostObjects LostObjects Buttons"
+    "RestituedObjects ResponseDelay ResponseDelay Reviews"
+    "Responses Types . .";
+}
 
+.time-saved-widget {
+  grid-area: TimeSaved;
+}
+
+.b {
+  grid-area: LostObjects;
+}
+
+.c {
+  grid-area: Buttons;
+}
 </style>
---
+++ src/components/TextWidget.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+type TextWidgetProps = {
+  title: string;
+  icon?: string;
+}
+
+const { title, icon } = defineProps<TextWidgetProps>();
+</script>
+
+<template>
+  <v-card elevation="2" :title="title" :prepend-icon="icon">
+    <v-divider></v-divider>
+    <v-card-text>
+      <slot></slot>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
